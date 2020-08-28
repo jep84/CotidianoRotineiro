5d80ac26b0149c76d0373dd92738bc40cd4c3fcd
Eduardo Porte <duporte@gmail.com>
2020-08-28 09:23:38 -0300

    add ip network ip var

--- findup.sh
+++ findup.sh
@@ -8,20 +8,22 @@ then
     exit 0
 fi
 
+IPNETWORK="${1%.*}"
+
 for i in `seq 1 254`
 do
-    echo -n "-> Checking IP ${1%.*}.${i} ... "
+    echo -n "-> Checking IP ${IPNETWORK}.${i} ... "
 
-    fping -q -c 1 -t 200 "10.203.33.${i}" 1>/dev/null 2>/dev/null
+    fping -q -c 1 -t 200 "${IPNETWORK}.${i}" 1>/dev/null 2>/dev/null
 
     if [ $? -eq 0 ]
     then
-        arping -D -I enp11s0 -c 2 "10.203.33.${i}" 1>/dev/null 2>/dev/null
+        arping -D -I enp11s0 -c 2 "${IPNETWORK}.${i}" 1>/dev/null 2>/dev/null
 
         if [ $? -eq 0 ]
         then
             echo "[DUP]"
-            echo "10.203.33.${i}" >> ipdup.txt
+            echo "${IPNETWORK}.${i}" >> ipdup.txt
         else
             echo "[OK]"
         fi
